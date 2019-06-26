2f9e16edd3e86d9c78035eaad7d2207d17aed9e4
mr.zang <13803895859@163.com>
2019-06-26 11:47:04 +0800

    change index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,66 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+ 
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
+
+app.post('/file_upload', function (req, res) {
+ 
+    console.log(req.files[0]);  // 上传的文件信息
+    
+    var des_file = __dirname + "/res/image/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+         fs.writeFile(des_file, data, function (err) {
+          if( err ){
+               console.log( err );
+          }else{
+                response = {
+                    message:'File uploaded successfully', 
+                    filename:req.files[0].originalname
+               };
+           }
+           console.log( response );
+           res.end( JSON.stringify( response ) );
+        });
+    });
+ })
+  
+
 // 在 app 文件夹开启静态服务
 app.use(express.static('../app'));
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+//  POST 请求
+app.post('/', function (req, res) {
+    console.log("主页 POST 请求");
+    res.send('Hello POST');
+})
+
+//  /del_user 页面响应
+app.get('/del_user', function (req, res) {
+    console.log("/del_user 响应 DELETE 请求");
+    res.send('删除页面');
+})
+
+//  /list_user 页面 GET 请求
+app.get('/list_user', function (req, res) {
+    console.log("/list_user GET 请求");
+    res.send('用户列表页面');
+})
+
+// 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
+app.get('/ab*cd', function (req, res) {
+    console.log("/ab*cd GET 请求");
+    res.send('正则匹配');
+})
+
 app.listen(3000, () => {
-    console.log('Demo server listening on port 8090');
+    console.log('Demo server listening on port 3000');
 });
